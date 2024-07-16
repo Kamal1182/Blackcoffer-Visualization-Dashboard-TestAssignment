6d4d9fb0dfa7bb1fd47caa0e9c2a60a3fbc727af
Kamal <mkamal1182@gmail.com>
2024-07-16 15:13:27 +0300

    Update api.service.ts

--- dashboard/dashboard-front/src/app/shared/services/api/api.service.ts
+++ dashboard/dashboard-front/src/app/shared/services/api/api.service.ts
@@ -1,9 +1,48 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, retry, throwError } from 'rxjs';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  private baseUrl = environment.apiUrl;
+  
+  constructor(private http: HttpClient) { }
+
+  private headers = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+    })
+  };
+
+  get(url: string) : Observable<any> {
+    return this.http.get(`${this.baseUrl}/${url}`, this.headers)
+      .pipe(
+        retry(1),
+        catchError((res: any) => {
+          return this.onRequestError(res);})
+      )
+      ;
+  }
+
+  onRequestError(res: any) {
+    const statusCode = res.status;
+    const body = res;
+
+    const error = {
+      statusCode : statusCode,
+      error : body.error.error
+    }
+
+    console.log('from api.service.ts')
+    console.log(error);
+
+    throwError(error);
+
+    return Observable.create((e: any) => {e.next(error)});
+  }
 }
+
